15a21300725817d2ba0fbd4ccf8f500c83bc3d83
FROL <nkt.frlv7@yandex.ru>
2025-01-18 20:09:28 +0100

    Finish Standart - START NEW ROOLES, server

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ const corsOptions = {
   methods: ['GET', 'POST'],
   credentials: true,
 };
+
 app.use(cors(corsOptions)); // Применяем CORS к приложению
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: corsOptions,
 });
@@ -37,6 +39,13 @@ io.on('connection', (socket) => {
         socket.emit('allowed');
         // Присваиваем роль: первый игрок "X", второй "O"
         socket.on('readyForRole', ({ name, room }) => {
+
+            // Проверка: если игрок уже есть в комнате, не добавляем его снова
+            const existingPlayer = rooms[room].find((player) => player.id === socket.id);
+            if (existingPlayer) {
+                console.log(`Игрок ${name} уже зарегистрирован в комнате ${room}`);
+                return;
+            }
             const role = rooms[room].length === 0 ? 'X' : 'O';
             // Добавляем игрока в комнату с его ролью
             const player = { id: socket.id, name, role };
@@ -57,6 +66,34 @@ io.on('connection', (socket) => {
         socket.on('restartGame', ({room}) => {
             io.to(room).emit('gemeRestarted');
         });
+
+        socket.on('leaveRoom', ({ name, room }) => {
+          // Проверяем существование комнаты
+          if (!rooms[room]) {
+              console.log(`Комната ${room} не найдена для выхода.`);
+              return;
+          }
+      
+          // Удаляем игрока из комнаты
+          rooms[room] = rooms[room].filter((player) => player.id !== socket.id);
+      
+          // Оповещаем остальных игроков
+          io.to(room).emit('gemeRestarted');
+          io.to(room).emit('updatePlayers', rooms[room]);
+          console.log(`${name} покинул комнату ${room}`);
+      
+          // Удаляем комнату, если она пуста
+          if (rooms[room].length === 0) {
+              delete rooms[room];
+              console.log(`Комната ${room} удалена, так как в ней больше нет игроков.`);
+          }
+      
+          // Отключаем сокет от комнаты
+          socket.leave(room);
+      
+          // Удаляем любые связанные с сокетом данные (если есть)
+          delete socket.room;
+      });
     
         socket.on('disconnect', () => {
             console.log('Игрок отключился:', socket.id);
@@ -72,13 +109,13 @@ io.on('connection', (socket) => {
                 io.to(room).emit('updatePlayers', rooms[room]);
               }
             }
-          });
+        });
     });
   });
   
 
   const PORT = process.env.PORT || 8080;
-  
+
   server.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
   });
